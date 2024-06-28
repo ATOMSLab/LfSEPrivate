e970ef47b9e3b80ed8be37bfdaf4e6df0b78250a
Alan Vithayathil <110578780+ALANV09304@users.noreply.github.com>
2024-06-28 16:05:51 -0400

    Update Pythagorean_theorem.lean

--- Lectures/Lecture 2 - Equalities and Inequalities/Pythagorean_theorem.lean
+++ Lectures/Lecture 2 - Equalities and Inequalities/Pythagorean_theorem.lean
@@ -1,1 +1,8 @@
-
+example {ladder base building : ℝ} (h1 : ladder = 100) (h2 : base = 16)
+ (h3 : building^2 = ladder^2 - base^2) : building^2 < 10000 := by
+  calc
+    building^2 = ladder^2 - base^2 := by rw [h3]
+    _ = 100^2 - base^2 := by rw [h1]
+    _ = 100^2 - 16^2 := by rw [h2]
+    _ = 9744 := by ring
+    _ < 10000 := by norm_num
